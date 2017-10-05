3c0f08074f4689c4bffe90870329ca5c7a091fde
Simon Emanuel Schmid <simon@schmid.io>
2017-10-05 09:50:43 +0200

    Setup prettier/eslint

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import Web3 from "web3";
 import { getFundForManager, setup } from "@melonproject/melon.js";
 
+const tracer = ({ timestamp, message, category, data }) => {
+  const args = [timestamp.toISOString(), `[${category}]`, message];
+  console.log(...args);
+};
+
 setup.init({
   web3: new Web3(new Web3.providers.HttpProvider("http://localhost:8545")),
-  daemonAddress: "0x00360d2b7d240ec0643b6d819ba81a09e40e5bcd",
   defaultAccount: "0x00360d2b7D240Ec0643B6D819ba81A09e40E5bCd",
-  tracer: ({ timestamp, message, category, data }) => {
-    const args = [timestamp.toISOString(), `[${category}]`, message];
-    console.log(...args);
-  }
+  tracer
 });
 
 (async () => {
